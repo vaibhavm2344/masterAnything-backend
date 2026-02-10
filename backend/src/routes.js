ba700bf2b99f783b35a19a0d2c215ecb233f4ac3
Vaibhav More <vaibhavcmore23@gmail.com>
2026-02-10 15:53:09 +0530

    add course and planner routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import authRoutes from './modules/auth/auth.routes.js';
 import { getMe } from './modules/auth/auth.controller.js';
-import authMiddleware, { requireRole } from './middlewares/auth.middleware.js';
+import authMiddleware from './middlewares/auth.middleware.js';
+import coursesRoutes from "./modules/courses/courses.routes.js";
+import plannerRoutes from "./modules/planners/planner.routes.js";
 
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.get('/me', authMiddleware, getMe);
-// router.get('/admin',authMiddleware, requireRole('admin'),adminController)
+router.use('/me', authMiddleware, getMe);
+router.use("/courses", coursesRoutes);
+router.use("/planners", plannerRoutes);
 
 export default router;
