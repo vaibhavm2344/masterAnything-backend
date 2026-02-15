a1d3145ed122adb75780daa228178219b280ad65
Vaibhav More <vaibhavcmore23@gmail.com>
2026-02-15 21:51:07 +0530

    solve redis connection error

--- backend/src/config/queue.js
+++ backend/src/config/queue.js
@@ -1,10 +1,5 @@
-import { Queue } from "bullmq";
-
 export const redisConnection = {
-  host: "127.0.0.1",
+  host: "redis",
   port: 6379,
   maxRetriesPerRequest: null,
 }
-export const aiQueue = new Queue("ai-tasks", {
-  connection: redisConnection
-});
